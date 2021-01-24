bbb509c8642c5f37c4ecdd9c225aba754c9981a4
Joshua Malone <jcharlesmalone@gmail.com>
2021-01-24 07:54:37 -0800

    "3" ~

--- Develop/index.js
+++ Develop/index.js
@@ -6,8 +6,45 @@ inquirer
         name: 'Project',
         message: 'Project Title',
     },
-    {},
-
+    {
+        type:'input',
+        name:'Description',
+        message:'Describe your project?',
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:'',
+    }, {
+        type:'input',
+        name:'usage',
+        message:'',
+    }, {
+        type:'input',
+        name:'contributing',
+        message:'',
+    },
+    {
+        type:'input',
+        name:'Test',
+        message:'',
+    },
+    {
+        type:'list',
+        name:'license',
+        message:'',
+    },
+    {
+        type:'input',
+        name:'gitHub',
+        message:'GitHub account name',
+    },
+    {
+        type:'input',
+        name:'email',
+        message:'What is your Email?',
+    },
+])
 // function to write README file
 function writeToFile(fileName, data) {
 }
